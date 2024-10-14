74b4d50ce57af5364f75183a168deed4f7066e7e
PACO630 <4b3g0128@stust.edu.tw>
2024-10-14 09:18:24 +0800

    Initial Commit

---
+++ n*n/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  n*n
+//
+//  Created by paco on 2024/10/14.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
